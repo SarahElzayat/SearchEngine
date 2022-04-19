8ce259abc49f0d804e857880636a3d92a18a0f31
Sarah Elzayat <sarahelzayat@outlook.com>
2022-04-19 16:56:15 +0200

    ..
    Retrieving from Database
    Maintaining its state
    TODO
    Normalization
    Threading

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 import java.net.URL;
 
-import com.mongodb.BasicDBObject;
-import groovy.lang.GString;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
 import org.bson.Document;
 import com.mongodb.client.*;
 import com.mongodb.client.MongoClient;
@@ -12,7 +12,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import org.jsoup.Jsoup;
-import com.mongodb.client.model.Projections;
+
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
@@ -21,9 +21,11 @@ import javax.print.Doc;
 public class HelloWorld {
 
   public static void main(String[] args) throws IOException {
-
+    /////////////////////// ADD CHECK FOR STATUS 1 FUNCTION////////////////////////
+    ///////////////////////// NORMALIZATION////////////////////////////////////////
     String uri = "mongodb://localhost:27017";
     HashSet<String> urls = new HashSet<>();
+    HashSet<String> urlsBGD = new HashSet<>();
     File myObj =
         new File(
             "D:\\CMP #2\\Second Semester\\Advanced Programming Techniques\\IntelliJ\\SearchEngine\\seed.txt");
@@ -31,33 +33,29 @@ public class HelloWorld {
     MongoDatabase db = mongo.getDatabase("SearchEngine");
     MongoCollection<Document> fetchedURLS = db.getCollection("FetchedURLs");
     MongoCollection<Document> URLSWithHTML = db.getCollection("URLSWithHTML");
+    long URLSWithHTMLID = URLSWithHTML.countDocuments();
 
-    //loop collection --> fill hashset
-    for (int i=0; i<fetchedURLS.countDocuments(); i++){
-      BasicDBObject dbObject = new BasicDBObject();
-      dbObject.put("_id",i);
-      FindIterable<Document> d =  fetchedURLS.find(dbObject);
-     for(Document doc : d)
-     {
-       urls.add((String) doc.get("_url"));
-     }
+    for (int i = 0; i < URLSWithHTML.countDocuments(); i++) {
+      FindIterable<Document> ddd = URLSWithHTML.find();
+      for (Document ff : ddd) {
+        urlsBGD.add((String) ff.get("_url"));
+      }
     }
-    //for (int i=0; i<URLSWithHTML.countDocuments(); i++){
-      System.out.println(urls);
-   // }
-
+    System.out.println(urlsBGD);
+    long fetchedURLSID = fetchedURLS.countDocuments();
     Scanner myReader = new Scanner(myObj);
-    int id=0;
-    int fetchedURLID=0;
-    for (int i = 0; i < 5; i++) // read from seeds.txt
+
+    for (int i = 0; i < 6; i++) // read from seeds.txt
     {
 
       String title;
       title = myReader.nextLine();
+      if (urlsBGD.contains(title)) continue;
       URL currentURL = new URL(title);
 
       String robotsURL =
-          (new URL(currentURL.getProtocol() + "://" + currentURL.getHost() + "/robots.txt")).toString();
+          (new URL(currentURL.getProtocol() + "://" + currentURL.getHost() + "/robots.txt"))
+              .toString();
       InputStream in = new URL(robotsURL).openStream();
       Scanner robots7aga = new Scanner(in).useDelimiter("\\A");
       String result = robots7aga.hasNext() ? robots7aga.next() : "";
@@ -79,39 +77,156 @@ public class HelloWorld {
 
       org.jsoup.nodes.Document doc;
       try {
-
         doc = Jsoup.connect(title).get();
       } catch (Exception e) {
         continue;
       }
+      if (!doc.toString().contains("<!doctype html>")) continue;
 
-      Document s = new Document("_id", id++).append("_url",title).append("html", doc.toString());
+      Document s =
+          new Document("_id", URLSWithHTMLID++)
+              .append("_url", title)
+              .append("html", doc.toString());
       // state = n --> not downloaded yet
       URLSWithHTML.insertOne(s);
+      urlsBGD.add(title);
 
       Elements el = doc.select("a[href]");
 
       for (Element lis : el) {
         Boolean tmam = true;
-        if (!urls.contains(lis.attr("href"))) {
-          for (int y = 0; y < disallows.size(); y++) {
-            if (lis.attr("href").contains(disallows.get(y))) {
-              tmam = false;
-            }
+        //        if (!urls.contains(lis.attr("href"))) {
+        for (int y = 0; y < disallows.size(); y++) {
+          if (lis.attr("href").contains(disallows.get(y))
+              && lis.attr("href").contains(currentURL.getHost())) {
+            tmam = false;
           }
-          if (!tmam) continue;
+        }
+        if (!tmam) continue;
 
-          urls.add(lis.attr("href"));
+        //          urls.add(lis.attr("href"));
 
-          if (lis.attr("href").contains("https")) {
+        if (lis.attr("href").contains("https")) {
 
-            Document link = new Document("_id", fetchedURLID++).append("_url",lis.attr("href"));
-            fetchedURLS.insertOne(link);
+          Document link =
+              new Document("_id", fetchedURLSID++)
+                  .append("_url", lis.attr("href"))
+                  .append("_state", 0); // 0 added, 1 being processed, 2 done
+          fetchedURLS.insertOne(link);
+        }
+        //        }
+      }
+    }
 
+    // ####################################################//
+    while(true){
+    Document find0 = new Document("_state", 1);
 
+    Document increase = new Document("$inc", new Document("_state", -1));
+
+    FindOneAndUpdateOptions options = new FindOneAndUpdateOptions();
+//    options.upsert(true);
+    options.returnDocument(ReturnDocument.AFTER);
+    Document coco=(Document) fetchedURLS.findOneAndUpdate(find0,increase,options);
+    if(coco==null)
+      break;
+
+    }
+    for (int i = 0; i < 100; i++) // read from seeds.txt
+    {
+
+      Document find0 = new Document("_state", 0);
+
+      Document increase = new Document("$inc", new Document("_state", 1));
+
+      FindOneAndUpdateOptions options = new FindOneAndUpdateOptions();
+      options.upsert(true);
+      options.returnDocument(ReturnDocument.AFTER);
+      Document coco = (Document) fetchedURLS.findOneAndUpdate(find0, increase, options);
+      int currentID = Integer.parseInt(coco.get("_id").toString());
+      Document find1 = new Document("_id", currentID);
+      String title;
+      title = coco.get("_url").toString();
+      System.out.println("URL coco " + title);
+      // return;
+      if (urlsBGD.contains(title)) {
+        fetchedURLS.findOneAndUpdate(find1, increase, options);
+        System.out.println("COCO ALREADY EXISTS");
+        continue;
+      }
+      URL currentURL = new URL(title);
+
+      String robotsURL =
+          (new URL(currentURL.getProtocol() + "://" + currentURL.getHost() + "/robots.txt"))
+              .toString();
+      InputStream in = new URL(robotsURL).openStream();
+      Scanner robots7aga = new Scanner(in).useDelimiter("\\A");
+      String result = robots7aga.hasNext() ? robots7aga.next() : "";
+      String[] array = result.split("\n"); // array of robots.txt as strings
+      Vector<String> disallows = new Vector<String>(0);
+
+      for (int x = 0; x < array.length; x++) {
+        if (array[x].contains("User-agent: *")) {
+          x++;
+
+          while (x < array.length && array[x].contains("Disallow")) {
+            array[x] = array[x].replace("Disallow: ", "");
+            disallows.add(array[x]);
+
+            x++;
           }
         }
       }
+
+      org.jsoup.nodes.Document doc;
+      try {
+        doc = Jsoup.connect(title).get();
+      } catch (Exception e) {
+        continue;
+      }
+      if (!doc.toString().contains("<!doctype html>")) {
+        fetchedURLS.findOneAndUpdate(find1, increase, options);
+        System.out.println("COCO ISN'T HTML");
+        continue;
+      }
+
+      Document s =
+          new Document("_id", URLSWithHTMLID++)
+              .append("_url", title)
+              .append("html", doc.toString());
+      // state = n --> not downloaded yet
+      URLSWithHTML.insertOne(s);
+
+      urlsBGD.add(title);
+
+      Elements el = doc.select("a[href]");
+
+      for (Element lis : el) {
+        Boolean tmam = true;
+
+        for (int y = 0; y < disallows.size(); y++) {
+          if (lis.attr("href").contains(disallows.get(y))
+              && lis.attr("href").contains(currentURL.getHost())) {
+            tmam = false;
+          }
+        }
+        if (!tmam) {
+          fetchedURLS.findOneAndUpdate(find1, increase, options);
+          System.out.println("NOT TMAM");
+          continue;
+        }
+
+        if (lis.attr("href").contains("https")) {
+          Document link =
+              new Document("_id", fetchedURLSID++)
+                  .append("_url", lis.attr("href"))
+                  .append("_state", 0); // 0 added, 1 being processed, 2 done
+          fetchedURLS.insertOne(link);
+        }
+
+      }
+      fetchedURLS.findOneAndUpdate(find1, increase, options);
+      System.out.println("Finished coco");
     }
   }
 }
