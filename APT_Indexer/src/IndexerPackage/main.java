31ad3a4f033331b55dd604a1b6dd5444b1bfbcb5
zeinabmoawad <Zeinab.hassan00@eng-st.cu.edu.eg>
2022-04-26 22:29:56 +0200

    QP before stemming

--- APT_Indexer/src/IndexerPackage/main.java
+++ APT_Indexer/src/IndexerPackage/main.java
@@ -5,6 +5,6 @@ public class main {
     {
 
        Indexer d=new Indexer();
-       d.Index("https://en.wikipedia.org/wiki/STL_(file_format)");
+       d.Index("https://www.tutorialspoint.com/cplusplus/cpp_stl_tutorial.htm");
     }
 }
--- APT_Indexer/src/QueryProcessing/queryprocessing.java
+++ APT_Indexer/src/QueryProcessing/queryprocessing.java
@@ -8,10 +8,7 @@ import org.bson.conversions.Bson;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 import org.json.JSONObject;
 import org.json.JSONArray;
@@ -28,16 +25,43 @@ public class queryprocessing {
     {
         String[] words = (QP_str).toLowerCase().split("\\s");//splits the string based on whitespace
         JSONArray []docarr=new JSONArray[words.length];
-        for (int i=0;i< words.length;i++) {
+        for (int i=0;i< words.length;i++)
+        {
             Document DBresult = database.collection.find(new Document("_id",words[i])).first();
-            if (DBresult != null) {
+
+            if (DBresult != null)
+            {
                 JSONObject obj = new JSONObject(DBresult.toJson());
                 JSONArray arr = obj.getJSONArray("DOC");
                   docarr[i]=arr;
-//                System.out.println(docarr[i].getJSONObject(0).getString("url"));
+              // System.out.println(docarr[i].getJSONObject(0).getString("url"));
             }
         }
-//        System.out.println(Arrays.toString(docarr));
+        Vector<JSONObject> result=new Vector<JSONObject>(docarr[0].length());
+        for (int i=0;i<docarr[0].length() ;i++)
+        {
+            String url=docarr[0].getJSONObject(i).getString("url");
+            boolean comm=false;
+            for (int j=1;j<docarr.length;j++)
+            {
+                comm=false;
+                for(int k=0;k<docarr[j].length();k++)
+                {
+                 comm=false;
+                    if((url.equals(docarr[j].getJSONObject(k).getString("url"))))
+                    {
+                        comm=true;
+                        break;
+                    }
+                }
+            }
+            /////common for all words
+            if(comm)
+            {
+               result.add(docarr[0].getJSONObject(i));
+            }
+        }
+//        System.out.println(Arrays.toString(result));
     }
 
 }
