5f45e69125d8e53f429969b29b303b6d4164d022
Sarah Elzayat <sarahelzayat@outlook.com>
2022-05-20 19:12:47 +0200

    .

--- demo/src/main/java/com/example/demo/Ranker.java
+++ demo/src/main/java/com/example/demo/Ranker.java
@@ -59,25 +59,25 @@ public class Ranker {
             float internalRank = 0;
             System.out.println("URL " + url);
             if (weights.has("h1"))
-                headers += weights.getJSONArray("h1").length() * 10;
+                headers += weights.getJSONArray("h1").length() * 12;
 
             if (weights.has("h2"))
-                headers += weights.getJSONArray("h2").length() * 8;
+                headers += weights.getJSONArray("h2").length() * 6;
 
             if (weights.has("h3"))
-                headers += weights.getJSONArray("h3").length() * 6;
+                headers += weights.getJSONArray("h3").length() * 3;
 
             if (weights.has("h4"))
-                headers += weights.getJSONArray("h4").length() * 4;
+                headers += weights.getJSONArray("h4").length() * 1.5;
 
             if (weights.has("h5"))
-                headers += weights.getJSONArray("h5").length() * 2;
+                headers += weights.getJSONArray("h5").length() * 1;
 
             if (weights.has("h6"))
-                headers += weights.getJSONArray("h6").length() * 2;
+                headers += weights.getJSONArray("h6").length() * 1;
 
             if (weights.has("p"))
-                headers += weights.getJSONArray("p").length();
+                headers += weights.getJSONArray("p").length()*.5;
 
             internalRank = headers;
             System.out.println("HEADERS " + internalRank);
@@ -86,7 +86,7 @@ public class Ranker {
             //internalRank += DF.get(i) / 5000.0;
             internalRank *= Math.log(5000.0 / NoofURLS);
             System.out.println("AFTER DF " + internalRank);
-            internalRank *= Integer.parseInt(shosho.get(url).get("popularity").toString()) * .001;
+            internalRank *= Integer.parseInt(shosho.get(url).get("popularity").toString()) ;
             System.out.println("AFTER POPULARITY " + internalRank);
             rank += internalRank;
 
@@ -125,22 +125,22 @@ public class Ranker {
                     headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h1").length() * 12;
 
                 if (weights.get(i).getJSONObject(searchWords.get(i)).has("h2"))
-                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h2").length() * 10;
+                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h2").length() * 6;
 
                 if (weights.get(i).getJSONObject(searchWords.get(i)).has("h3"))
-                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h3").length() * 8;
+                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h3").length() * 3;
 
                 if (weights.get(i).getJSONObject(searchWords.get(i)).has("h4"))
-                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h4").length() * 6;
+                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h4").length() * 1.5;
 
                 if (weights.get(i).getJSONObject(searchWords.get(i)).has("h5"))
-                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h5").length() * 4;
+                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h5").length() * 1;
 
                 if (weights.get(i).getJSONObject(searchWords.get(i)).has("h6"))
-                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h6").length() * 2;
+                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("h6").length() * 1;
 
                 if (weights.get(i).getJSONObject(searchWords.get(i)).has("p"))
-                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("p").length();
+                    headers += weights.get(i).getJSONObject(searchWords.get(i)).getJSONArray("p").length()*.5;
                 internalRank = headers;
                 System.out.println("Headers " + internalRank);
                 internalRank /= Integer.parseInt(shosho.get(url).get("NoOfWords").toString());
